658d05aba8e907e570714c2177daef8548065d6d
Sean Kennedy <snkennedy21@gmail.com>
2022-05-06 12:34:35 -0400

    add keyboard functionality

--- script.js
+++ script.js
@@ -1,70 +1,136 @@
 "use strict";
 const boardTiles = document.querySelectorAll(".board__tile");
-const boardRows = document.querySelectorAll(".board__row");
+const allBoardRows = document.querySelectorAll(".board__row");
+const keyboard = document.querySelector(".keyboard");
 const keyboardButons = document.querySelectorAll(".keyboard__button");
 const keyboardButtonBackspace = document.querySelector(
   ".keyboard__button--backspace"
 );
 const keyboardButtonEnter = document.querySelector(".keyboard__button--enter");
 
-const boardRow1 = document.querySelector(".board__row__1");
-const boardRow2 = document.querySelector(".board__row__2");
-const boardRow3 = document.querySelector(".board__row__3");
-const boardRow4 = document.querySelector(".board__row__4");
-const boardRow5 = document.querySelector(".board__row__5");
-const boardRow6 = document.querySelector(".board__row__6");
+class App {
+  #guessArray = [];
+  #answerArray = ["W", "A", "T", "E", "R"];
+  #rowIndex = 0;
+  #tiles;
+  #pressedKey;
+  #guessArrayIsFull = false;
+  #playerGuessMatchesTheAnswer = false;
+  #currentRowOfPlay;
+  #allTilesInCurrentRowOfPlay;
+  constructor() {
+    keyboard.addEventListener("click", this._playTheGame.bind(this));
+    window.addEventListener("keydown", this._playTheGame.bind(this));
+  }
 
-let guessArray = [];
-let answerArray = ["W", "A", "T", "E", "R"];
-
-let rowIndex = 0;
-
-let tiles = boardRows[rowIndex].querySelectorAll(".board__tile");
-
-const updateTiles = function () {
-  rowIndex++;
-  tiles = boardRows[rowIndex].querySelectorAll(".board__tile");
-};
-
-keyboardButons.forEach((button) => {
-  button.addEventListener("click", function () {
-    guessArray.push(button.value);
-    if (guessArray.length > 5) {
-      guessArray.pop();
+  _playTheGame(e) {
+    this._identifyCurrentRowOfPlay();
+    this._selectTilesInCurrentRowOfPlay();
+    this._identifyWhichKeyWasPressed(e);
+    if (this.#pressedKey === "Enter") {
+      this._checkIfGuessArrayIsFull();
+      console.log(this.#guessArray);
+      if (!this.#guessArrayIsFull) alert("not complete word");
+      if (this.#guessArrayIsFull) {
+        this._submitPlayerGuess();
+        this._increaseRowIndex();
+        this._resetGuessArrayToEmpty();
+      }
     }
-    console.log(guessArray);
+    if (this.#pressedKey === "Backspace") {
+      this._removeLastLetterFromGuessArray();
+      console.log(this.#guessArray);
+      this._setTheTextContentForTilesInCurrentRowOfPlay();
+    }
+    if (this.#pressedKey !== "Enter" && this.#pressedKey !== "Backspace") {
+      this._addLetterOfPressedKeyToGuessArray();
+      console.log(this.#guessArray);
+      this._checkIfGuessArrayIsFull();
+      if (this.#guessArrayIsFull) this._stopAddingLettersToGuessArray();
+      this._setTheTextContentForTilesInCurrentRowOfPlay();
+    }
+  }
 
-    tiles.forEach((tile, i) => {
-      tile.textContent = guessArray[i];
-    });
-  });
-});
+  _checkIfPlayerGuessMatchesTheAnswer() {
+    if (this.#guessArray.every((el, i) => el === this.#answerArray[i])) {
+      this.#playerGuessMatchesTheAnswer = true;
+    }
+  }
 
-keyboardButtonBackspace.addEventListener("click", function () {
-  guessArray.pop();
-  console.log(guessArray);
-  tiles.forEach((tile, i) => {
-    tile.textContent = guessArray[i];
-  });
-});
-
-keyboardButtonEnter.addEventListener("click", function () {
-  if (guessArray.length === 5) {
-    tiles.forEach((tile, i) => {
-      if (tile.textContent !== answerArray[i]) {
+  _updateTileColors() {
+    this.#allTilesInCurrentRowOfPlay.forEach((tile, i) => {
+      if (tile.textContent !== this.#answerArray[i]) {
         tile.style.backgroundColor = "grey";
         tile.style.color = "white";
+        tile.style.border = "none";
       }
-      if (answerArray.includes(tile.textContent)) {
-        tile.style.backgroundColor = "yellow";
+      if (this.#answerArray.includes(tile.textContent)) {
+        tile.style.backgroundColor = "#ffc425";
         tile.style.color = "white";
+        tile.style.border = "none";
       }
-      if (tile.textContent === answerArray[i]) {
-        tile.style.backgroundColor = "green";
+      if (tile.textContent === this.#answerArray[i]) {
+        tile.style.backgroundColor = "#008000";
         tile.style.color = "white";
+        tile.style.border = "none";
       }
     });
-    guessArray = [];
-    updateTiles();
   }
-});
+
+  _submitPlayerGuess() {
+    this._updateTileColors();
+    this._checkIfPlayerGuessMatchesTheAnswer();
+  }
+
+  _resetGuessArrayToEmpty() {
+    this.#guessArray = [];
+  }
+
+  _increaseRowIndex() {
+    this.#rowIndex++;
+  }
+
+  _removeLastLetterFromGuessArray() {
+    this.#guessArray.pop();
+  }
+
+  _identifyWhichKeyWasPressed(e) {
+    e.preventDefault();
+    if (e.type === "keydown") {
+      this.#pressedKey = e.key[0].toUpperCase() + e.key.slice(1);
+    }
+    if (e.type === "click") {
+      this.#pressedKey = e.target.closest(".keyboard__button").value;
+    }
+  }
+
+  _addLetterOfPressedKeyToGuessArray() {
+    this.#guessArray.push(this.#pressedKey);
+  }
+
+  _checkIfGuessArrayIsFull() {
+    if (this.#guessArray.length < 5) this.#guessArrayIsFull = false;
+    if (this.#guessArray.length >= 5) this.#guessArrayIsFull = true;
+  }
+
+  _stopAddingLettersToGuessArray() {
+    if (this.#guessArray.length > 5) this.#guessArray.pop();
+  }
+
+  _identifyCurrentRowOfPlay() {
+    this.#currentRowOfPlay = allBoardRows[this.#rowIndex];
+  }
+
+  _selectTilesInCurrentRowOfPlay() {
+    this.#allTilesInCurrentRowOfPlay =
+      this.#currentRowOfPlay.querySelectorAll(".board__tile");
+  }
+
+  _setTheTextContentForTilesInCurrentRowOfPlay() {
+    this.#allTilesInCurrentRowOfPlay.forEach((tile, i) => {
+      tile.textContent = this.#guessArray[i];
+    });
+  }
+}
+
+const app = new App();
